04954cc51b0039a5f21bf1efd7fb33e0f3d81abd
Benitk <31191253+Benitk@users.noreply.github.com>
2020-11-10 09:42:15 -0800

    Initial commit

---
+++ app/src/main/java/com/example/biddle/MainActivity.java
@@ -0,0 +1,14 @@
+ipackage com.example.biddle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
