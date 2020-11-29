b6f08634f64feaf6457be51f36bc3fdb9ef96af7
AviBoter <maccavi3@gmail.com>
2020-11-30 01:06:45 +0200

    my Versia

--- app/src/main/java/com/example/biddle/Activites/SellerActivity.java
+++ app/src/main/java/com/example/biddle/Activites/SellerActivity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.biddle.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,7 +17,6 @@ public class SellerActivity extends AppCompatActivity {
 
     private Button menu_seller_btn;
     private Button product_publish_btn;
-    private Button show_my_products_btn;
     private FirebaseAuth firebaseAuth;
 
     @Override
@@ -26,19 +28,12 @@ public class SellerActivity extends AppCompatActivity {
 
         menu_seller_btn = (Button) findViewById(R.id.menu_seller_btn);
         product_publish_btn = (Button) findViewById(R.id.product_publication_btn);
-        show_my_products_btn = (Button) findViewById(R.id.show_my_products_btn);
+
 
         product_publish_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //move to publish product activity
-            }
-        });
-
-        show_my_products_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //should take all seller's products fro DB
             }
         });
 
@@ -48,7 +43,36 @@ public class SellerActivity extends AppCompatActivity {
                 //want to open the menu from the left side
             }
         });
+        class MyAdapter extends BaseAdapter {
 
+            // override other abstract methods here
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return position;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup container) {
+                if (convertView == null) {
+                    convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+                }
+
+                ((TextView) convertView.findViewById(android.R.id.text1))
+                        .setText(getItem(position));
+                return convertView;
+            }
+        }
 
 
 
